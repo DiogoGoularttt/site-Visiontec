c6a976d5c13ccd4151d7c8254794fee061f0eada
DiogoGoularttt <diogosgoulart@icloud.com>
2023-10-18 21:01:53 -0300

    ➕ Product carousel styling

--- js/carrossel.js
+++ js/carrossel.js
@@ -1,33 +1,38 @@
 $(document).ready(function () {
-    $(".js-carrossel").slick({
+    // Inicialize o carrossel
+    $('.js-carrossel').slick({
         dots: false,
-        infinite: false,
-        speed: 200,
-        arrows: false,
-        slidesToShow: 6,
-        slidesToScroll: 4,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
         responsive: [{
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 3,
-                },
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
             },
             {
                 breakpoint: 600,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 2,
-                },
+                    slidesToScroll: 2
+                }
             },
             {
                 breakpoint: 480,
                 settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                },
-            },
-        ],
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 });
 
